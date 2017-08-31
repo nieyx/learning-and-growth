d0dbd48c559c5b480d8b14adde4d34d118c1efac
nieyx <nieyx91@outlook.com>
2017-08-31 16:45:14 +0800

    update chartroom

--- websocket/demo/chartRoom/wsServer.js
+++ websocket/demo/chartRoom/wsServer.js
@@ -7,16 +7,25 @@ var server = ws.createServer(function(conn){
 	// 创建一个变量，用来标记当前server的使用者
 	clientCount ++;
 	conn.nickName = 'user' + clientCount;
-	broadcast(conn.nickName + 'comes in');
+	var mes = {};
+	mes.type = 'enter';
+	mes.content = conn.nickName + 'comes in';
+	broadcast(JSON.stringify(mes));
 	conn.on('text', function(str){
 		console.log('received message' + str);
 		// 定义一个 broadcast 的方法，来通知ws下所有的客户端新的连接进入服务器
-		broadcast(conn.nickName + ' : '+ str);
+		var mes = {};
+		mes.type = 'message';
+		mes.content = conn.nickName + ':' + str;
+		broadcast(JSON.stringify(mes));
 	});
 
 	conn.on('close', function(){
 		console.log('connection closed');
-		broadcast(conn.nickName + 'leave');
+		var mes = {};
+		mes.type = 'leave';
+		mes.content = conn.nickName + 'leave';
+		broadcast(JSON.stringify(mes));
 	});
 
 	conn.on('error', function(err){
