f5e65ba9b38f1c7200cb760b58ce69cc23a24482
nieyx <nieyx91@outlook.com>
2017-09-05 23:44:20 +0800

    grunt 准备工作

--- node/moiveApp/app.js
+++ node/moiveApp/app.js
@@ -1,97 +1,166 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var Movie = require('./models/movie');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 // process是node的全局变量，可以获得端口信息
 var port = process.env.PORT || 3000;
 //  web服务器
 var app = express();
-
-// 设置师徒的根目录
+app.locals.moment = require('moment');// c传入本地的数据库
+// mongoose.createConnection('mongodb://localhost/movieApp');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/movieApp', {server: { poolSize: 5 }, useMongoClient: true});
+// 设置视图的根目录
 app.set('views', './views/pages');
 // 设置默认的模版引擎
 app.set('view engine', 'jade');
-
+app.use(bodyParser.json()); // for parsing application/json
+ 
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//http://www.expressjs.com.cn/guide/using-template-engines.html
+// 设置静态资源的 默认路径
+app.use(express.static(path.join(__dirname,'/public')));
 // 监听端口
 app.listen(port);
 
 // 在控制台输出，看是否可以成功的启动服务
 console.log('movie started on port ' + port);
 
-
-// 设置静态资源的 默认路径
-app.use(express.static(path.join(__dirname,'/public')));
 // 主要页面的路由
 app.get('/',  function(req, res){
-	res.render('index', {
-		title: 'movieApp 首页',
-		movies: [
-          { 
-          	title:"机械战警",
-          	_id:1,
-          	poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
+  Movie.fetch(function(err, movies){
+    if (err){
+      console.log(err);
+    }
 
-          },
-          {
-            title:"X战警",
-          	_id:2,
-          	poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
-          },
-          {
-          	title:"皇家骑士",
-          	_id:3,
-          	poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
-
-          }
-     	]
-	});
+    res.render('index', {
+      title: 'movieApp 首页',
+      movies: movies
+    });
+  });
+	
 });
-
+// 详情页信息
 app.get('/movie/:id',  function(req, res){
-	res.render('detail', {
-		title: 'movieApp detail详情页',
-		movie: {
-           doctor:'何塞.帕迪利亚',
-           country:"美国",
-           title:"机械战警",
-           year:2014,
-           poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5",
-           language:"英语",
-           flash:"http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf",
-           summary:"《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。"
-          }
-
-	});
+  // 通过url的参数值，就可以获取到id
+  var id = req.params.id;
+  Movie.findById(id, function(err, movie){
+    res.render('detail', {
+      title: 'movieApp' + movie.title,
+      movie: movie
+    });
+  });
 });
-
+// 后台录入页
 app.get('/admin/movie',  function(req, res){
 	res.render('admin', {
 		title: 'movieApp admin后台录入页',
 		movie:{
-           title:"",
-           doctor:"",
-           country:"",
-           year:"",
-           poster:"",
-           flash:"",
-           summary:"",
-           language:""
-        } 
+      title:"",
+      doctor:"",
+      country:"",
+      year:"",
+      poster:"",
+      flash:"",
+      summary:"",
+      language:""
+    } 
 	});
 });
 
-app.get('/admin/list',  function(req, res){
-	res.render('list',{
-        title:'imooc 列表页',
-        movies:[{
-        title:"机械战警",
-        _id:1,
-        doctor:'何塞.帕迪利亚',
-        country:"美国",
-        year:2014,
-        poster:"http://r3.ykimg.com/05160000530EEB63675839160D0B79D5",
-        language:"英语",
-        flash:"http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf",
-        summary:"《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。"
+//admin update movie,拿到数据后更新表单
+app.get('/admin/update/:id',function(req,res){
+   var id = req.params.id;
+   if(id){
+      Movie.findById(id,function(err,movie){
+        res.render('admin',{
+            title:'imooc 后台更新页',
+            movie:movie
+        });
+      });
+   }
+
+});
+
+// 后台录入页提交表单之后，数据内容的存储
+app.post('/admin/movie/new', function(req, res){
+  // 从表单提交的数据，可能是新添加的，页可能是修改之后再提交的
+  // 所以要做个判断，判断是哪种情况
+  var id = req.body.movie._id;
+  var movieObj = req.body.movie;
+  console.log(req.body);
+  // 修改原有之后的提交
+  if (id !== 'undefined'){
+    Movie.findById(id, function(err, movie){
+      if (err){
+        console.log(err);
+      }
+
+      _movie = _.extend(movie, movieObj);
+      _movie.save(function(err, movie){
+        if (err){
+          console.log(err);
         }
-        ]
+        // 数据更新完成，重定向到详情页
+        res.redirect('/movie/' + movie._id);
+      });
     });
+
+
+  } else {
+    _movie = new Movie({
+      doctor:movieObj.doctor,
+      title:movieObj.title,
+      country:movieObj.country, 
+      language:movieObj.language,
+      year:movieObj.year,
+      poster:movieObj.poster, 
+      summary:movieObj.summary,
+      flash:movieObj.flash     
+    });
+
+    _movie.save(function(err,movie){
+        if (err) {
+            console.log(err);
+        }
+
+        res.redirect('/movie/' + movie._id);
+    });
+
+  }
+});
+
+
+// 列表页
+app.get('/admin/list',  function(req, res){
+	Movie.fetch(function(err, movies){
+    if (err){
+      console.log(err);
+    }
+
+    res.render('list', {
+      title: 'movieApp 列表页',
+      movies: movies
+    });
+  });
+});
+
+// 删除逻辑路由
+app.delete('/admin/list', function(req, res){
+  // 从请求的url来获取id值，获得id值之后，可以将数据从数据库中删除
+  var id = req.query.id;
+  if (id){
+    Movie.remove({_id:id}, function(err, movie){
+      if (err) {
+        console.log(err);
+      } else {
+        // 当没有错误的时候，返回json数据，返回1
+        res.json({success: 1});
+      }
+
+
+    });
+  }
 });
